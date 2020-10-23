e5fc2c4b5c24123e8b387faf8e5ca8bf947b4b84
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-23 16:55:19 -0400

    add: iterate over data for links/social icons

--- src/data.js
+++ src/data.js
@@ -4,49 +4,49 @@ export const links = [
   {
     id: 1,
     url: '/',
-    text: 'random',
+    text: 'home'
   },
   {
     id: 2,
     url: '/about',
-    text: 'about',
+    text: 'about'
   },
   {
     id: 3,
     url: '/projects',
-    text: 'projects',
+    text: 'projects'
   },
   {
     id: 4,
     url: '/contact',
-    text: 'contact',
+    text: 'contact'
   },
   {
     id: 5,
     url: '/profile',
-    text: 'profile',
-  },
+    text: 'profile'
+  }
 ]
 
 export const social = [
   {
     id: 1,
     url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
+    icon: <FaFacebook />
   },
   {
     id: 2,
     url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
+    icon: <FaTwitter />
   },
   {
     id: 3,
     url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
+    icon: <FaLinkedin />
   },
   {
     id: 4,
     url: 'https://www.twitter.com',
-    icon: <FaBehance />,
-  },
+    icon: <FaBehance />
+  }
 ]
